28cb319ef879afd9f63b4e0db7d6c537350ddfb1
PedroAbreu04 <pedroabreudealmeida@gmail.com>
2026-03-01 23:18:53 -0300

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import logo from './assets/logo.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav>
+        <div>
+          <img className='logo' src={logo} alt="Logo Kiyan" />
+        </div>
+        <div className='menu'>
+          <ul>
+            <li><a href="#">Sobre nos</a></li>
+            <li><a href="#">Produtos</a></li>
+            <li><a href="#">Certificações</a></li>
+            <li><a href="#">Representantes</a></li>
+          </ul>
+        </div>
+        <div className='faleConosco'>
+          <ul>
+            <li><a href="#">Fale Conosco</a></li>
+          </ul>
+        </div>
+      </nav>
     </>
   )
 }
